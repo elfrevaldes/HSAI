930594b56b0d096fbf5c5f08abc75920e31330b1
Ricardo Goncalves <grosa82@gmail.com>
2016-05-05 09:40:08 -0600

    Add click method

--- HSAI/Form1.Designer.cs
+++ HSAI/Form1.Designer.cs
@@ -33,19 +33,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(67, 117);
+            this.button1.Location = new System.Drawing.Point(75, 146);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(84, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(317, 316);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- HSAI/Form1.cs
+++ HSAI/Form1.cs
@@ -12,9 +12,18 @@ namespace HSAI
 {
     public partial class Form1 : Form
     {
+        IntPtr hsgame;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hsgame = HearthstoneHelper.GetHearthstoneWindow();
+
+            ClickOnPointTool.ClickOnPossition(hsgame, new Point(1000, 700));
         }
     }
 }
